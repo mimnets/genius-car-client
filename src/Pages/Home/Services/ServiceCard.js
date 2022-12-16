6592a87a44e60d2de73422c27063eca7d37bf0cb
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-17 00:15:56 +0600

    re-touch the service page

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -7,7 +7,7 @@ const ServiceCard = ({service}) => {
         <figure><img src={img} alt="Shoes" /></figure>
         <div className="card-body">
             <h2 className="card-title">{title}</h2>
-            <p>Price: {price}</p>
+            <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
             <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
             </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
     },[])
     return (
         <div>
-            <div className='text-center'>
+            <div className='text-center mb-4'>
             <p className='text-2xl font-bold text-orange-600'> Service</p>
             <h2 className="text-5xl font-semibold">Our Service Area</h2>
             <p className=''>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
