7778f12114ad7642507595cfbf26bfc81a02feb5
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-16 10:53:52 +0600

    added border radious

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,6 @@ import img3 from '../../../assets/images/banner/3.jpg';
 import img4 from '../../../assets/images/banner/4.jpg';
 import img5 from '../../../assets/images/banner/5.jpg';
 import img6 from '../../../assets/images/banner/6.jpg';
-import './Banner.css';
 import BannerItem from './BannerItem';
 
 const bannerData = [
@@ -51,9 +50,9 @@ const bannerData = [
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full py-10">
           {
-            bannerData.map(slide => <BannerItem slide={slide}></BannerItem>)
+            bannerData.map(slide => <BannerItem key={slide.id} slide={slide}></BannerItem>)
           }
           {/* <BannerItem image={img1}></BannerItem>
         <div id="slide2" className="carousel-item relative w-full">
