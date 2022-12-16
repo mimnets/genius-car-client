cfc8b4904c5e0f09be878d66b1bb42504f5ad716
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-17 00:12:30 +0600

    added service page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <h2>This is home</h2>
             <Banner></Banner>
             <About></About>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    return (
+        <div>
+            <div className='text-center'>
+            <p className='text-2xl font-bold text-orange-600'> Service</p>
+            <h2 className="text-5xl font-semibold">Our Service Area</h2>
+            <p className=''>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            </div>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services; 
