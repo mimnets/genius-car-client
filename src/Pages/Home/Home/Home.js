1329fbccadeef6fc4d321f4688088dcd1b69b89e
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-16 11:20:37 +0600

    edit about section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from '../About/About';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <h2>This is home</h2>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
