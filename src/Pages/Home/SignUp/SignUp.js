5f95a3f0d4d4c66c0573372246b22c5bccf03e65
mdmonirulislammonir@gmail.com <mdmonirulislammonir@gmail.com>
2022-12-17 19:20:19 +0600

    Login and sign up page added

--- src/Pages/Home/SignUp/SignUp.js
+++ src/Pages/Home/SignUp/SignUp.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../../assets/images/login/login.svg';
 
-const Login = () => {
-    const handleLogin = event =>{
+const SignUp = () => {
+    const handleSignup = event =>{
         event.preventDefault();
     }
-
     return (
         <div className="hero w-full">
         <div className="hero-content grid md:grid-cols-2 flex-col">
@@ -14,8 +13,14 @@ const Login = () => {
             <img className='w-3/4' src={img} alt=""/>
             </div>
             <div className="card w-full mx-20 max-w-sm shadow-2xl bg-base-100 py-20">
-            <h1 className="text-5xl text-center font-bold">Login</h1>
+            <h1 className="text-5xl text-center font-bold">Sign Up</h1>
             <form className="card-body">
+                <div className="form-control">
+                <label className="label">
+                    <span className="label-text">Name</span>
+                </label>
+                <input type="text" placeholder="name" className="input input-bordered" />
+                </div>
                 <div className="form-control">
                 <label className="label">
                     <span className="label-text">Email</span>
@@ -27,19 +32,16 @@ const Login = () => {
                     <span className="label-text">Password</span>
                 </label>
                 <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
                 </div>
                 <div className="form-control mt-6">
-                <input onSubmit={handleLogin} type='submit' className="btn btn-primary" value="Login"/>
+                <input onSubmit={handleSignup} type='submit' className="btn btn-primary" value="Signup"/>
                 </div>
             </form>
-            <p className='text-center'>New to Genius Car <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
+            <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Login</Link></p>
             </div>
         </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
